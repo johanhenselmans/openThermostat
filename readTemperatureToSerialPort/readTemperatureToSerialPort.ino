188e15d2160ce4dea09790283258ee1d19c6b391
J. Miguel P. Tavares <migtavares@gmail.com>
2013-05-16 00:02:20 +0200

    Fix wrong funtion names.

--- readTemperatureToSerialPort/readTemperatureToSerialPort.ino
+++ readTemperatureToSerialPort/readTemperatureToSerialPort.ino
@@ -1,3 +1,6 @@
+/* Small program that read temperature from the sensor
+ * and prints it to the serial port. */
+
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
@@ -8,21 +11,23 @@ OneWire oneWire(ARDUINO_BUS);
 
 DallasTemperature tempSensor(&oneWire);
 
-void loop(void) {
+void setup(void) {
   Serial.begin (9600);
   tempSensor.begin ();
   Serial.println ("OpenThermostat setup complete");
+  pinMode (LED_PIN, OUTPUT);
 }
 
-void setup(void) {
+void loop(void) {
+  digitalWrite (LED_PIN, HIGH);
+  delay (1000);
+  digitalWrite (LED_PIN, LOW);
+  delay (1000);
+
+  Serial.println (" Readind temp");
   tempSensor.requestTemperatures ();
   long temp = tempSensor.getTempCByIndex (0);
   Serial.print (temp);
-  Serial.println (" C");
-  
-  digitalWrite (LED_PIN, HIGH);
-  delay (500);
-  digitalWrite (LED_PIN, LOW);
-  delay (1000);
+  Serial.println (" C");  
 }
 
